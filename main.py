8d7d064fcde71ff40f8cfd2e744570c2ba29f4c7
Waseem Khan <waseemkhan96@outlook.com>
2016-01-22 20:46:22 -0500

    changed up command order

--- main.py
+++ main.py
@@ -9,11 +9,11 @@ while True:
   helpText = '''  'x' to exit
   'h' for help
   'R' to read full text
-  'LineNumber;Operation;Content' to edit (args are seperated by semicolons) 
+  'Operation;LineNumber;Content' to edit (args are seperated by semicolons) 
     'd' operation to delete line (needs Line#)
     'r' operation to read line (needs Line#)
     'w' operation to splice in line after line number(needs Line# and Content)
-      ex) 3:d, 15:r, 16:w:Hello World!'''
+      ex) d:3, r:15, w:16:Hello World!'''
   if command=='x' or command == 'quit' or command == 'exit':
     f.close()
     sys.exit("editing complete")
@@ -25,15 +25,16 @@ while True:
     for i in range(len(text)):
       print str(i) + ' ' + text[i],
   else:
-    pars = re.search('(\d+);(\w);*(.*)',command)
+    pars = re.search('(\w);(\d+);*(.*)',command)
     if pars:
-      lineNum = int(pars.group(1))
-      oper = pars.group(2)
+      lineNum = int(pars.group(2))
+      oper = pars.group(1)
       content = pars.group(3)
       f.seek(0)
       if oper == 'r': print text[lineNum],
       elif oper == 'd': 
-        print 'del@' + str(lineNum) + ':' + text.pop(lineNum),
+        delLine = text.pop(lineNum)
+        print 'del@' + str(lineNum) + ':' + delLine,
       elif oper == 'w': text.insert(lineNum+1, content)
       if oper != 'r':
         for i in range(len(text)): f.write(text[i])
